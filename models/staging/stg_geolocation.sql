e7482b1cc0d283821d423f360e8fc3bf6999c255
Maithili Wade <129838254+maithwade@users.noreply.github.com>
2026-07-07 20:24:59 -0400

    geolocation

---
+++ models/staging/stg_geolocation.sql
@@ -0,0 +1,16 @@
+with source as (
+    select * from {{ source('raw', 'geolocation') }}
+),
+
+renamed as (
+    select
+        geolocation_zip_code_prefix as zip_code_prefix,
+        geolocation_lat as latitude,
+        geolocation_lng as longitude,
+        geolocation_city as city,
+        geolocation_state as state
+        
+    from source
+)
+
+select * from renamed
